9019f93b5ca2ed7a1775d4e370b4843972b6a3c7
Jeff Hoskins <jdvhoskins@users.noreply.github.com>
2018-03-22 17:05:46 -0400

    Initial commit

---
+++ corpus.py
@@ -0,0 +1,6 @@
+import numpy as np
+from nltk.corpus import PlaintextCorpusReader
+
+#corpus_root = '/usr/share/dict' [1]
+#>>> wordlists = PlaintextCorpusReader(corpus_root, '.*') [2]
+#>>> wordlists.fileids()
