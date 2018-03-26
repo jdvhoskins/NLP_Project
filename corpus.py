ea287fcb282e7fca1c70419db7d54fd4c88e850f
Jeff Hoskins <jdvhoskins@users.noreply.github.com>
2018-03-26 19:43:32 -0400

    Delete corpus.py

--- corpus.py
+++
@@ -1,6 +0,0 @@
-import numpy as np
-from nltk.corpus import PlaintextCorpusReader
-
-#corpus_root = '/usr/share/dict' [1]
-#>>> wordlists = PlaintextCorpusReader(corpus_root, '.*') [2]
-#>>> wordlists.fileids()
